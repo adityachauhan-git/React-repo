18f1b413109134d5336b7084598e09633f633934
adityachauhan200079-collab <adityachauhan200079@gmail.com>
2026-06-29 18:02:56 +0530

    making a counter

---
+++ To-do App/front-end/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+
+function App() {
+    const [count, setCount]= useState(0);
+
+    function incrementClick(){
+        setCount(prevCount=>{
+          return ++prevCount;
+        })
+    }
+    function decrementClick(){
+        setCount(prevCount=>{
+          return --prevCount;
+        })
+    }
+
+    return(
+      <div>
+      <button onClick={incrementClick}>+</button>
+      <p>{count}</p>
+      <button onClick={decrementClick}>-</button>
+
+
+
+      <h1>This is count: {count}</h1>
+      </div>
+    )
+}
+
+
+
+export default App
